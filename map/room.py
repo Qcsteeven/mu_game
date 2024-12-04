a743aaae18550d23cd4f82f4f8e72dc70887b58a
Dmitro <kreck58@bk.ru>
2024-12-04 16:13:37 +0300

    create rooms

--- map/room.py
+++ map/room.py
@@ -1,3 +1,16 @@
 class Room:
-    def __init__(self):
+    def __init__(self, isSafe: bool, creations: list[Entity], objects: list[Objects], roomEffect: list[roomEffect]):
+        self.isSafe = isSafe
+        self.creations = creations
+        self.objects = objects
+        self.roomEffect = roomEffect
+
+    
+    def update(self):
+        pass
+    
+    def applyEffect(self):
+        pass
+
+    def dischargeEffect(self):
         pass
