f5d74b7d8beb1ae0aaeb126a94897e14ae726f9b
Quirence <shades.h4ndeir@gmail.com>
2024-12-05 03:40:48 +0800

    notify_port

--- management/game_manager.py
+++ management/game_manager.py
@@ -1,3 +1,7 @@
+import json
+import random
+
+
 class GameManager:
     _instance = None  # Приватное поле для хранения единственного экземпляра
     isCreated = False
@@ -13,9 +17,22 @@ class GameManager:
         GameManager.isCreated = True
         # self._map = Map(settings)
         # self._entities = []
-        self._map = ["Пидор"]
-        pass
+        self._map = ["Карта"]
+        try:
+            with open('text.JSON', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                self.ending_phrases = data.get("ending", [])
+        except FileNotFoundError:
+            print("Файл с данными не найден.")
+        except json.JSONDecodeError:
+            print("Ошибка при чтении JSON файла.")
 
-    def notify_port(self, notify_message):
+    def update(self, notify_message, *args, **kwargs):
         # В зависимости от сообщения, сделать какое-то взаимодействие игры и персонажа
-        pass
+        match notify_message:
+            case "lose":
+                if self.ending_phrases:
+                    phrase = random.choice(self.ending_phrases)["dialogue"]
+                    print(phrase)
+                else:
+                    print("Нет доступных концовочных фраз.")
