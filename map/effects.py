d663c1cbeea83b53e8362dcfcd99f78450d5f16a
Rostik <rostanzatoplaev@gmail.com>
2024-12-04 22:46:09 +0300

    new version effects

--- map/effects.py
+++ map/effects.py
@@ -1,20 +1,35 @@
 import random
-from random import choice
+from collections import defaultdict
+from typing import Callable
+
 
 class Effect:
-    effect_room = ["dark", "treasury", "healing", "lock"]
+    effects_room = ["dark", "treasury", "healing", "lock"]
 
-    def __init__(self, type_room):
-        self.type_room = type_room
+    def __init__(self, dict_rooms_determined_effect : dict):
+        self.dict_rooms_determined_effect = dict_rooms_determined_effect # комнаты, которые точно будут с этим эффектом
 
-    # number_room - номер комнаты, на которую нужно накинуть определенный эффект-type_room
-    def apple_effect_room(self, graph : list[[]], type_room, number_room = -1):
+    def __call__(self, func : Callable)->Callable:
+        def wrapper(rooms : list[list]):
+            for i in range(0, len(rooms)):
+                for j in range(0, len(rooms[i])):
+                    rooms[i][j] = random.choice(Effect.effects_room)
+               #     rooms[i][j].applyEffect()  # обновляем эффект для каждой комнаты
 
-        for i in range(0, len(graph)):
-            for j in range(0, len(graph[i])):
-                graph[i][j] = random.choice(Effect.effect_room)
+            # заполняем комнаты с определенным эффектом
+            for number_room in self.dict_rooms_determined_effect.keys():
+                rooms[number_room - 1][0] = self.dict_rooms_determined_effect.get(number_room)
+               # rooms[number_room].applyEffect()
 
-        if number_room != -1:
-            graph[number_room] = type_room
+            return func(self)
 
-        print(*graph)
+        return wrapper
+
+my_dict = {1: "dark", 2: "treasury", 3: "healing", 4: "lock"}
+my_rooms = [[None, None, None], [None, None, None], [None, None, None]]
+
+@Effect(my_dict)
+def apply_effect(rooms : list[list]):
+    print("эффекты")
+
+apply_effect(my_rooms)
