99302c377233013bccb9ad78f8c5624b56603507
Quirence <shades.h4ndeir@gmail.com>
2024-12-04 20:20:44 +0800

    singleton added

--- management/game_manager.py
+++ management/game_manager.py
@@ -1,3 +1,14 @@
 class GameManager:
-    def __init__(self):
-        pass
+    _instance = None  # Приватное поле для хранения единственного экземпляра
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameManager, cls).__new__(cls)
+        return cls._instance
+
+
+Manager1 = GameManager()
+print("Object created, ", Manager1)
+
+Manager2 = GameManager()
+print("Object created, ", Manager2)
