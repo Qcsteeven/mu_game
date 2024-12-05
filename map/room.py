8b7cdc85de1c5fbccdcf22af69949df34975170a
Rostik <rostanzatoplaev@gmail.com>
2024-12-05 15:04:58 +0300

    normis effects rooms

--- map/room.py
+++ map/room.py
@@ -1,57 +1,66 @@
 from abc import ABC, abstractmethod
+from effects import Effect
+
+
 class Room(ABC):
-            def __init__(self, isSafe: bool, creations: list[Entity], objects: list[Objects], roomEffect: list[roomEffect]):
-                self.isSafe = isSafe
-                self.creations = creations
-                self.objects = objects
-                self.roomEffect = roomEffect
-        
-            @abstractmethod
-            def update(self) -> None:
-                    pass
-            @abstractmethod
-            def applyEffect(self) -> None:
-                    pass
-            @abstractmethod
-            def dischargeEffect(self) -> None:
-                    pass
+    def __init__(self, isSafe: bool, creations: list[Entity], objects: list[Objects], roomEffect=None):
+        self.isSafe = isSafe
+        self.creations = creations
+        self.objects = objects
+        self.roomEffect = roomEffect
 
-class  Armory(Room):
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def applyEffect(self) -> None:
+        pass
+
+    @abstractmethod
+    def dischargeEffect(self) -> None:
+        pass
+
+
+class Armory(Room):
     type = "Armory"
+
     def update(self):
-           for creation in self.creations:
-                  creation.update()
+        for creation in self.creations:
+            creation.update()
 
-           for obj in self.objects:
-                  obj.update()
-          
-           for effect in self.roomEffect:
-                  effect.update()
+        for obj in self.objects:
+            obj.update()
+
+        for effect in self.roomEffect:
+            effect.update()
 
 
-class  Shop(Room):
+class Shop(Room):
     type = "Shop"
+
     def update(self):
-           for creation in self.creations:
-                  creation.update()
+        for creation in self.creations:
+            creation.update()
 
-           for obj in self.objects:
-                  obj.update()
+        for obj in self.objects:
+            obj.update()
 
 
-class  Dangeon(Room):
+class Dangeon(Room):
     type = "Dangeon"
-    def update(self):
-           for creation in self.creations:
-                  creation.update()
 
-           for obj in self.objects:
-                  obj.update()
-          
+    def update(self):
+        for creation in self.creations:
+            creation.update()
+
+        for obj in self.objects:
+            obj.update()
+
+    @Effect()
     def applyEffect(self):
-           for effect in self.roomEffect:
-                  effect.update()
+        for effect in self.roomEffect:
+            effect.update()
 
     def dischargeEffect(self):
-           self.roomEffect = None
-
+        self.roomEffect = None
