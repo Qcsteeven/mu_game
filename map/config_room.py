8b31f1f96dfad9899fa6a5c9dc9632f4e3ea8617
Shalaginov Semyon <semashal@yandex.ru>
2024-12-05 13:50:03 +0300

    Added connections between game_manager and map

--- map/config_room.py
+++ map/config_room.py
@@ -2,27 +2,11 @@ import json
 
 
 class Adapter:
-    @classmethod
-    def adapt(cls, path="config.json"):
+    @staticmethod
+    def adapt(path="config.json"):
         with open(path, "r") as rfile:
             data = json.load(rfile)
-        height = data["options"]["height"]
-        width = data["options"]["width"]
-        print(type(width))
-        pmap = data["map"]
-        count = 0
-        counth = 0
-        arr = [[] * width for i in range(height)]
-        for i in pmap.values():
-            if count == width:
-                counth += 1
-                count = 0
-            if counth == height:
-                break
-            arr[counth].append(i)
-            print(count)
-            count += 1
-        return arr
+        return data["settings"]
 
 
-Adapter.adapt()
+
--- map/create_map.py
+++ map/create_map.py
@@ -2,37 +2,25 @@ import json
 import random
 
 
-def create_2d_array(rows, cols, d_count):
+class PatCreator:
+    @staticmethod
+    def create_2d_array(rows, cols, d_count):
 
-    array = [['-' for _ in range(cols)] for _ in range(rows)]
+        array = [['-' for _ in range(cols)] for _ in range(rows)]
 
-    array[0][0] = 's'
-    array[0][1] = 'a'
-    array[rows - 1][cols - 1] = 'B'
+        array[0][0] = 's'
+        array[0][1] = 'a'
+        array[rows - 1][cols - 1] = 'B'
 
-    positions = [(i, j) for i in range(rows) for j in range(cols) if array[i][j] == '-']
+        positions = [(i, j) for i in range(rows) for j in range(cols) if array[i][j] == '-']
 
-    for _ in range(d_count):
-        i, j = random.choice(positions)
-        array[i][j] = 'd'
-        positions.remove((i, j))
+        for _ in range(d_count):
+            i, j = random.choice(positions)
+            array[i][j] = 'd'
+            positions.remove((i, j))
 
-    return array
-
-
-def print_array(array):
-    for row in array:
-        print(' '.join(row))
+        return array
 
 
-with open("C:/Users/Admin/Desktop/popka_dimi/mu_game/map/config.json", "r") as file:
-    data = json.load(file)
-
-rows = data["options"]["rows"]
-cols = data["options"]["cols"]
-d_count = data["options"]["d_count"]
-
-array = create_2d_array(rows, cols, d_count)
-print_array(array)
 
       
