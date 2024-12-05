e2051a8dbbc3fbb60f45dcafa8d37ee7088195e0
Dmitro <kreck58@bk.ru>
2024-12-06 01:42:39 +0300

    new version running map

--- map/map.py
+++ map/map.py
@@ -1,8 +1,6 @@
 from map.room import *
 import random
-from entities.npc import trader, robber
-from management.game_manager import GameManager
-
+from entities.entity import Entity
 
 class Map:
     _instance = None
@@ -13,7 +11,7 @@ class Map:
             cls._instance = super(Map, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, pattern: list[list[str]], game_manager: GameManager):
+    def __init__(self, pattern: list[list[str]], Entities: list[Entity]):
         if Map._isCreated == True:
             return
         Map._isCreated = True
@@ -27,24 +25,17 @@ class Map:
         for i in range(leng):
             for j in range(len(pattern[i])):
                 if pattern[i][j] == 's':
-                    shop = Shop(True,[],[],[])
-                    tr = trader(game_manager)
-                    tr.room = shop
-                    shop.creations.append(tr)
-                    self._map[i].append(shop)
+                    self._map[i].append(Shop(True, [], [], []))
                 elif pattern[i][j] == 'a':
-                    armory = Armory(True, [], [], [])
-                    self._map[i].append(armory)
+                    self._map[i].append(Armory(True, [], [], []))
                 elif pattern[i][j] == 'd':
-                    dungeon = Dungeon(False,[],[],[])
-                    rb = robber(game_manager, random.choice["common", "rare", "epic"])
-                    rb.room = dungeon
-                    dungeon.creations.append(rb)
-                    self._map[i].append(dungeon)
+                    enemy = random.choice(Entities)
+                    self._map[i].append(Dungeon(False, [enemy], [], []))
                 elif pattern[i][j] == "B":
-                    self._map[i].append(EmptyRoom(True, [], [], []))
+                    self._map[i].append(BossRoom(True, [], [], []))
                 elif pattern[i][j] == '-':
                     self._map[i].append(EmptyRoom(True, [], [], []))
+                
         self._map[0][0].visited = True
 
     def get_room(self, x_pos: int, y_pos: int):
@@ -60,13 +51,14 @@ class Map:
     def map_size(self):
         return self.rows, self.cols
 
-    def show_map(self, pos) -> None:
-        x, y = pos
-        arr = [['?'] * self.cols for i in range(self.rows)]
+    def show_map(self, x, y) -> None:
+        arr = [['?'] * self.cols for _ in range(self.rows)]
+
         for i in range(self.rows):
             for j in range(self.cols):
                 if self._map[i][j].visited:
-                    arr[i][j]=self.pattern[i][j]
+                    arr[i][j] = self.pattern[i][j]
+        
         arr[x][y] = "X"
         for i in arr:
             print(*i)
